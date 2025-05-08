e7f52321ec9b40734fbf3956910d562c431e4754
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:31 +0930

    knightrider works. pep cleanups

--- led_grid_driver/knightrider.py
+++ led_grid_driver/knightrider.py
@@ -1,9 +1,10 @@
 from ledgridtemplate import LedGridTemplate
+from unicodedata import bidirectional
 
 class KnightRider(LedGridTemplate):
 
-    eye_c        = 0xFF0000 # red
-    background_c = 0x000000 # black
+    eye_c = 0xFF0000  # red
+    background_c = 0x000000  # black
 
 
     def init(self, strip, num_led):
@@ -14,19 +15,21 @@ class KnightRider(LedGridTemplate):
                current_cycle):
 
         height = self.get_y()
-        width  = self.get_x()
-        eye_width = int(height * 0.15) # 15 %
+        width = self.get_x()
+        eye_height = int(height * 0.15)  # 15 %
+        remainder_height = height - eye_height
+        half_steps = int(num_steps_per_cycle / 2)
 
-        # TODO - Not finished
-        offset = None
-        if (current_step < (num_steps_per_cycle/2)):
-            offset = current_step
+        offset = int(abs(half_steps - current_step) / half_steps * remainder_height)
+        offset_increasing = current_step >= half_steps
+
+        for i in range (eye_height):
+            self.set_line_y_rgb(strip, offset + i, self.eye_c)
+        if (offset_increasing):
+            self.set_line_y_rgb(strip, offset, self.background_c)
+            self.set_line_y_rgb(strip, offset - 1, self.background_c)
         else:
-            offset = height - eye_width - current_step
-        self.set_line_y_rgb(strip, -1+offset, self.background_c)
-        for y in range (eye_width):
-            self.set_line_y_rgb(strip, y+offset, self.eye_c)
-        self.set_line_y_rgb(strip, offset, self.background_c)
-        self.set_line_y_rgb(strip, 1+offset, self.background_c)
+            self.set_line_y_rgb(strip, offset + eye_height, self.background_c)
+            self.set_line_y_rgb(strip, offset + eye_height + 1, self.background_c)
 
         return 1
