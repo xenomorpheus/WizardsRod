6b1124191bca3b1bfb0e442d35c51aa033d234d2
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:38 +0930

    unit test fix

--- Brain/spelltrigger.py
+++ Brain/spelltrigger.py
@@ -13,6 +13,12 @@ class SpellTrigger():
     def __init__(self, trigger_type) -> None:
         self.trigger_type = trigger_type
 
+    def __eq__(self, other: 'StaffEvent'):
+        return self.__dict__ == other.__dict__
+
+    def __hash__(self):
+        return hash((self.trigger_type))
+
     def get_name(self) -> str:
         return self.get_trigger_type().get_name()
 
