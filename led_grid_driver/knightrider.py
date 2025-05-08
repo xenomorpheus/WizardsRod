5cb09fb96cf9fbf52fca1b8239c73d5379aaf889
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:35 +0930

    knightrider code improvements

--- led_grid_driver/knightrider.py
+++ led_grid_driver/knightrider.py
@@ -12,10 +12,10 @@ class KnightRider(LedGrid):
     def init(self, strip, num_led):
         """This method is called to initialize a colour program.
         """
-        height = self.get_y()
-        width = self.get_x()
-        eye_height = int(height * 0.15)  # 15 %
-        remainder_height = height - eye_height
+        self.height = self.get_y()
+        self.width = self.get_x()
+        self.eye_height = int(self.height * 0.15)  # 15 %
+        self.remainder_height = self.height - self.eye_height
 
     def update(self, strip, num_led, num_steps_per_cycle, current_step,
                current_cycle):
@@ -23,17 +23,18 @@ class KnightRider(LedGrid):
         half_steps = int(num_steps_per_cycle / 2)
 
         offset = int(abs(half_steps - current_step) /
-                     half_steps * remainder_height)
+                     half_steps * self.remainder_height)
         offset_increasing = current_step >= half_steps
 
-        for i in range(eye_height):
+        for i in range(self.eye_height):
             self.set_line_y_rgb(strip, offset + i, self.eye_c)
         if (offset_increasing):
             self.set_line_y_rgb(strip, offset, self.background_c)
             self.set_line_y_rgb(strip, offset - 1, self.background_c)
         else:
-            self.set_line_y_rgb(strip, offset + eye_height, self.background_c)
-            self.set_line_y_rgb(strip, offset + eye_height + 1,
+            self.set_line_y_rgb(strip,
+                                offset + self.eye_height, self.background_c)
+            self.set_line_y_rgb(strip, offset + self.eye_height + 1,
                                 self.background_c)
 
         return 1
