016bffea37b926699ee351ff73414bb3a969f284
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:45 +0930

    fix comments

--- Brain/brain/hardwarefetch.py
+++ Brain/brain/hardwarefetch.py
@@ -1,28 +1,28 @@
-'''
+"""
 Created on 19 Sep. 2019
 
 @author: bruins
-'''
+"""
 
 from buttoneventgenerator import ButtonEventGenerator
 
 
 class HardwareFetch():
-    '''
+    """
     HardwareFetch
-    '''
+    """
 
     def __init__(self):
-        '''
+        """
         Constructor
-        '''
+        """
         self.beg = ButtonEventGenerator()
 
     def __str__(self):
         return self.__class__.__name__
 
     def get(self, hint: str):
-        ''' return hardware object using hint as selector '''
+        """ return hardware object using hint as selector """
         if hint == 'BUTTON':
             return self.beg
         raise Exception("Hint "+hint+" not known")
