8520cd72a21f88953722828c4ec8a80d4ea31c4a
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:46 +0930

    better structure. added typing

--- Brain/brain/hardwarefetch.py
+++ Brain/brain/hardwarefetch.py
@@ -4,6 +4,8 @@ Created on 19 Sep. 2019
 @author: bruins
 """
 
+from typing import Dict
+from hardware import Hardware
 from buttoneventgenerator import ButtonEventGenerator
 
 
@@ -11,18 +13,19 @@ class HardwareFetch():
     """
     HardwareFetch
     """
+    generator = {}  # type: Dict[str, Hardware]
 
     def __init__(self):
         """
         Constructor
         """
-        self.beg = ButtonEventGenerator()
+        self.generator['BUTTON'] = ButtonEventGenerator()
 
     def __str__(self):
         return self.__class__.__name__
 
-    def get(self, hint: str):
+    def get(self, hint: str) -> Hardware:
         """ return hardware object using hint as selector """
-        if hint == 'BUTTON':
-            return self.beg
+        if hint in self.generator:
+            return self.generator[hint]
         raise Exception("Hint "+hint+" not known")
