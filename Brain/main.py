32624c8d690b42c610ad2e95e7a32b228d50dd2e
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:36 +0930

    Brain - refactored staff and prepared spell list code

--- Brain/main.py
+++ Brain/main.py
@@ -17,24 +17,7 @@ class main():
 
         fireball = Spell("Fireball").setTriggerSequence(fireball_trigger_sequence).setTriggerTimeout(6). \
             setHardwareSet([SpellHardwareConst.ACCELEROMETER])
-
-        # preparing a spell list
-        prepared_spells = SpellListPrepared('MyList')
-        prepared_spells.spellAddList([fireball])
-        # prepared_spells.spellAdd(spell4)
-        # prepared_spells.spellDel('spell name')
-
-        # Some spells might need special hardware, e.g. GPS
-        staff = Staff().setHardwareHints(prepared_spells.getHardwareHints())
-
-        # looking for staff events to trigger spells
-        loops = 2
-        while loops > 1:
-            prepared_spells.acceptEvents(staff.getNewStaffEvents())
-            for spell in prepared_spells.getTriggeredSpells():
-                spell.performActions(staff)
-            loops = loops - 1
-
+        staff = Staff("MyStaff").spellAdd(fireball).run()
 
 if __name__ == '__main__':
     main.main()
--- Brain/staff.py
+++ Brain/staff.py
@@ -1,15 +1,41 @@
+from spell import Spell
+from spelllistprepared import SpellListPrepared
+
 class Staff():
 
     def __init__(self, name: str) -> None:
         self.name = name
+        self.spell_list_prepared = SpellListPrepared(name + ' SpellListPrepared')
         self.hardware_hints = []
 
     def getName(self) -> str:
         return self.name
 
-    def setHardwareHints(self, hardware_hints) -> 'Staff':
-        self.hardware_hints = hardware_hints
+    def spellAdd(self, spell: Spell) -> 'Staff':
+        self.spell_list_prepared.spellAdd(spell)
+        self.__recalculate_hardware_hints()
         return self
 
-    def getNewStaffEvents(self):
+    def spellAddList(self, spelllist) -> 'Staff':
+        self.spell_list_prepared.spellAddList(spelllist)
+        self.__recalculate_hardware_hints()
+        return self
+
+    def __recalculate_hardware_hints(self) -> None:
+        hardware_hints_new = self.spell_list_prepared.getHardwareHints()
+        # TODO add/remove hardware
+        self.hardware_hints = hardware_hints_new
+
+    def __getNewStaffEvents(self):
+        # TODO Poll hardware_hints hardware for events
         return[] # TODO
+
+
+    def run(self) -> None:
+        # looking for staff events to trigger spells
+        loops = 2
+        while loops > 1:
+            self.spell_list_prepared.acceptEvents(self.__getNewStaffEvents())
+            for spell in self.spell_list_prepared.getTriggeredSpells():
+                spell.performActions(staff)
+            loops = loops - 1
