ad1b1d96b3a6d9db021879314bcc8040b9803034
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-05-08 20:59:41 +0930

    spell trigger tests working

--- Brain/tests/test_spelllistprepared.py
+++ Brain/tests/test_spelllistprepared.py
@@ -6,6 +6,7 @@ import unittest
 from spelllistprepared import SpellListPrepared
 from spell import Spell
 from spelltrigger import SpellTrigger
+from staff import Staff
 from staffevent import StaffEvent
 import const
 
@@ -16,7 +17,6 @@ class TestSpellListPrepared(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """ setup all tests """
-        cls.spell = const.MASTER_SPELLBOOK["TEST_SPELL_01"]
 
     def test_constructor(self):
         """ test """
@@ -25,47 +25,40 @@ class TestSpellListPrepared(unittest.TestCase):
 
     def test_spell_add(self):
         """ test """
+        test_spell01 = Spell('Test Spell 01')
         slp = SpellListPrepared(None)
-        slp_got = slp.spell_add(self.spell)
+        slp_got = slp.spell_add(test_spell01)
         self.assertEqual(slp, slp_got, 'builder pattern. must be object')
 
     def test_spell_add_list(self):
         """ test """
+        test_spell01 = Spell('Test Spell 01')
         slp = SpellListPrepared(None)
-        slp_got = slp.spell_add_list([self.spell])
+        slp_got = slp.spell_add_list([test_spell01])
         self.assertEqual(slp, slp_got, 'builder pattern. must be object')
 
     def test_spell_del(self):
         """ test """
+        test_spell01 = Spell('Test Spell 01')
         slp = SpellListPrepared(None)
-        slp_got = slp.spell_del(self.spell.name)
+        slp_got = slp.spell_del(test_spell01)
         self.assertEqual(slp, slp_got, 'builder pattern. must be object')
 
     def test_recieve_events_no_spells_no_events(self):
         """ test """
         slp = SpellListPrepared(None)
         new_events = []
+        self.perform_action_calls = set()
         slp.recieve_events(new_events)
+        self.assertFalse(self.perform_action_calls)
 
     def test_recieve_events_no_spells_some_events(self):
         """ test """
         slp = SpellListPrepared(None)
         new_events = [const.EVENT["TEST_01"], const.EVENT["TEST_02"]]
+        self.perform_action_calls = set()
         slp.recieve_events(new_events)
-
-    def test_recieve_events_some_spells_some_events(self):
-        """ test """
-        triggers = [
-            SpellTrigger('TEST_01'),
-            SpellTrigger('TEST_02')]
-        test_spell01 = Spell('Test Spell 01').set_trigger_sequence(triggers)
-        slp = SpellListPrepared(None).spell_add(test_spell01)
-        # Gesture Test03 will be ignored
-        events = [
-            StaffEvent('TEST_01', 4),
-            StaffEvent('TEST_02', 4),
-            StaffEvent('TEST_03', 4)]
-        slp.recieve_events(events)
+        self.assertFalse(self.perform_action_calls)
 
     # Only accept events that are for our prepared spells
     def test_recieve_events_some_spells_unwanted_events(self):
@@ -75,34 +68,45 @@ class TestSpellListPrepared(unittest.TestCase):
             SpellTrigger('TEST_02')]
         test_spell01 = Spell("Test Spell 01").set_trigger_sequence(triggers)
         slp = SpellListPrepared(None).spell_add(test_spell01)
-        events = [
+        new_events = [
             StaffEvent('Event 03', 4)]
-        slp.recieve_events(events)
-
-    def test_get_spells_triggered_no_spells_no_events(self):
-        """ test """
-        slp = SpellListPrepared(None)
-        new_events = []
+        self.perform_action_calls = set()
         slp.recieve_events(new_events)
+        self.assertFalse(self.perform_action_calls)
 
-    def test_get_spells_triggered_one_spells_no_events(self):
+    def test_recieve_events_one_spells_no_events(self):
         """ test """
-        slp = SpellListPrepared(None).spell_add(self.spell)
+        test_spell01 = Spell('Test Spell 01')
+        slp = SpellListPrepared(None).spell_add(test_spell01)
         new_events = []
+        self.perform_action_calls = set()
         slp.recieve_events(new_events)
+        self.assertFalse(self.perform_action_calls)
 
-    def test_get_spells_triggered_some_spells(self):
+    def perform_action(self, spell, staff):
+        """ detect if this method was called.
+        simulate the spell performing actions. """
+        self.perform_action_calls.add(staff.name+"="+spell.name)
+
+    def test_recieve_events_some_spells(self):
         """ test """
         triggers = [
             SpellTrigger('TEST_01'),
             SpellTrigger('TEST_02')]
-        test_spell01 = Spell("Test Spell 01").set_trigger_sequence(triggers)
-        slp = SpellListPrepared(None).spell_add(test_spell01)
+        test_spell01 = Spell("test_recieves_event_some_spells")
+        test_spell01.set_trigger_sequence(triggers)
+        test_spell01.set_perform_actions(self.perform_action)
+        staff = Staff("The Staff")
+        slp = SpellListPrepared(staff).spell_add(test_spell01)
         # Gesture Test03 will be ignored
         events = [
             StaffEvent('TEST_01', 4),
             StaffEvent('TEST_02', 4),
             StaffEvent('TEST_03', 4)]
+        self.perform_action_calls = set()
+        slp.recieve_events(events)
+        self.assertEqual(set([staff.name+"="+test_spell01.name]),
+                        self.perform_action_calls)
 
     def test_get_hardware_hints(self):
         """ test """
@@ -112,7 +116,7 @@ class TestSpellListPrepared(unittest.TestCase):
         slp = SpellListPrepared(None).spell_add_list([test_spell01,
                                                           test_spell02])
         hwl = slp.get_hardware_hints()
-        self.assertTrue(set(['Button01', 'Button02', 'Button03']), set(hwl))
+        self.assertEqual(set(['Button01', 'Button02', 'Button03']), set(hwl))
 
 
 if __name__ == '__main__':
